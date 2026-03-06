8391ab1b930556b285a874c2142839c28bd59bd4
chang404notfound <1746150790@qq.com>
2026-03-06 15:16:30 +0800

    feat:update hello.c

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
-int main(){
-  printf("hello,world!\n");
-  return 0;
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[50];
+    printf("请输入你的名字：");
+    scanf("%s", name);
+    printf("你输入的名字是：%s\n", name);
+    printf("Hello, %s! 欢迎进入嵌入式开发世界！\n", name);
+    return 0;
 }
